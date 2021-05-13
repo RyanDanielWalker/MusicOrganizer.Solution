ff8a9fcf38136f05c7d0ea30785ac261e197a035
Ryan Walker <ryandanielwalker@gmail.com>
2021-05-13 11:25:05 -0700

    Add Album controller

---
+++ MusicOrganizer/Controllers/AlbumsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using MusicOrganizer.Models;
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Controllers
+{
+  public class AlbumsController : Controller
+  {
+    [HttpGet("/albums/{albumId}/artists/new")]
+    public ActionResult New(int artistId)
+    {
+      Artist artist = Artist.Find(artistId);
+      return View(artist);
+    }
+    [HttpGet("/artists/{artistId}/albums/{albumId}")]
+    public ActionResult Show(int artistId, int albumId)
+    {
+      Album album = Album.Find(albumId);
+      Artist artist = Artist.Find(artistId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("album", album);
+      model.Add("artist", artist);
+      return View(model);
+    }
+    [HttpPost("/albums/delete")]
+    public ActionResult DeleteAll()
+    {
+      Album.ClearAll();
+      return View();
+    }
+  }
+}
